86e24fd08d5cd01459f43ef5002984bb40c3d09f
pcesaralb <pcesaralb@outlook.com>
2015-11-16 22:44:14 -0200

    Create BemVindo.php

---
+++ BemVindo.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>bem vindo</title>
+</head>
+
+<body>
+bem vindo 	<?php echo $_GET["firstname"]; ?><br>
+			<?php echo $_GET["lastname"]; ?>
+
+
+
+</body>
+</html>
